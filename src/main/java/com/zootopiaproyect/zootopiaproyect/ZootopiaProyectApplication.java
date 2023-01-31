564ad6e3e58f1645568139f45f04bebc397ef060
Ramiro-isla <ramiroisla11@gmail.com>
2023-01-31 15:06:37 +0100

    First Commit

---
+++ src/main/java/com/zootopiaproyect/zootopiaproyect/ZootopiaProyectApplication.java
@@ -0,0 +1,13 @@
+package com.zootopiaproyect.zootopiaproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZootopiaProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZootopiaProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zootopiaproyect/zootopiaproyect/ZootopiaProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zootopiaproyect.zootopiaproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZootopiaProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
